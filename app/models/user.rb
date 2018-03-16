72e7b7a6793dfbaedcdc94180daf7e3d9e1e1fe0
pbyra92 <peb992@gmail.com>
2018-03-16 15:54:58 -0400

    add_user_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
+  validates_presence_of :email, :encrypted_password, :role, :sign_in_count
+
   has_many :reviews
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+validates_format_of :email, :with => /\A[^@,\s]+@[^@,\s]+\.[^@,\s]+\z/
+  def admin?
+    role == 'admin'
+  end
 end
