517130fd3890d20367d14fa97ab5674146923196
4Rax <davegarber94@gmail.com>
2018-03-19 12:28:05 -0400

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315134842) do
+ActiveRecord::Schema.define(version: 20180316202734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20180315134842) do
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "thumbs_up_count", default: 0, null: false
+    t.integer "thumbs_down_count", default: 0, null: false
     t.index ["breed_id"], name: "index_reviews_on_breed_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
